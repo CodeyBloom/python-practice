72835822a9a4acf4de09c4f9817d373eceb53869
Codey Bloom <codey.bloom@protonmail.com>
2025-05-25 09:38:40 +1000

    getting some parameter testing practice

--- hello-pytest/hello.py
+++ hello-pytest/hello.py
@@ -1,15 +1,30 @@
 """For best practices with pytest I can separate my actions from computations, so I only need to call "capsys" one one
 test (the one testing the actions) not all of them. See the tests to grok it"""
 
+# this is like a little mock API for hello world, all evalutation
+import argparse
 
-# this is like a little mock API for hello world, all pure evalutation
-def output():
-    return "Hello, World!"
+
+def parse_args(arg_str: str | None = ""):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--greeting", type=str, default="Hello")
+    parser.add_argument("-n", "--name", type=str, default="World")
+
+    arg_list = None if arg_str is None else arg_str.split()
+
+    args = parser.parse_args(arg_list)
+
+    return args
+
+
+def output(arg_str: str | None = ""):
+    args = parse_args(arg_str)
+    return f"{args.greeting}, {args.name}!"
 
 
 # then this is like a little application that uses the API, with actions
-def main():
-    print(output())
+def main(arg_str: str | None = ""):
+    print(output(arg_str))
 
 
 if __name__ == "__main__":
--- hello-pytest/test_api.py
+++ hello-pytest/test_api.py
@@ -15,3 +15,26 @@ def test_hello_comma():
 def test_hello_ends_with():
     result = hello.output()
     assert result.endswith("!")
+
+
+# testing the parameters/args
+
+
+def test_greeting():
+    result = hello.output("-g Goodbye")
+    assert result == "Goodbye, World!"
+
+
+def test_long_greeting():
+    result = hello.output("--greeting Goodbye")
+    assert result == "Goodbye, World!"
+
+
+def test_name():
+    result = hello.output("-n Pytest")
+    assert result == "Hello, Pytest!"
+
+
+def test_long_name():
+    result = hello.output("--name Pytest")
+    assert result == "Hello, Pytest!"
